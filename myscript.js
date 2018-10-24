73c479fc53b7911d3e9da0e4e173f015b7669eb0
briansbotlab <qw7539@yahoo.com.tw>
2018-10-24 19:16:28 +0800

    20181024_2

--- myscript.js
+++ myscript.js
@@ -1,18 +1,45 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+//var ctx2 =canvas.getContext("2d");
 var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
+var x = 125;
+var y = 125;
 var dx = 2;
 var dy = 2;
 var u;
-var x1 = canvas.width/2;
-var y1 = canvas.height-30;
+var x1 = 20;
+var y1 = 30;
 var dx1 = 1;
-var dy1 = 1;
-
+var dy1 = -1;
+var random_int = Math.random();
 var button_state = 0;
 
+function small_ball() {
+        }
+        //画方块的方法
+        small_ball.prototype.draw = function() {
+            ctx.beginPath();
+            ctx.arc(x1, y1, 3, 0, Math.PI*2);
+            ctx.fillStyle = "#000000";
+            ctx.fill();
+            ctx.closePath();
+        }
+var small_ball = new small_ball()
+        
+function normal_ball() {
+        }
+        //画方块的方法
+        normal_ball.prototype.draw = function() {
+            ctx.beginPath();
+            ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+var normal_ball = new normal_ball()
+        
+        
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -22,18 +49,39 @@ function drawBall() {
 }
 
 
-function drawBall_2() {
-    ctx.beginPath();
-    ctx.arc(x1, y1, 3, 0, Math.PI*2);
-    ctx.fillStyle = "#AA00FF";
-    ctx.fill();
-    ctx.closePath();
+
+function draw2() {
+    //ctx.clearRect(x1, y1, 5, 5);
+    
+    //ctx.clearRect(x1-5, y1-5, 10, 10);
+    small_ball.draw();
+
+    if(x1 + dx1 > canvas.width-10 || x1 + dx1 < 10) {
+        dx1 = -dx1;
+    }
+    if(y1 + dy > canvas.height-5 || y1 + dy1 < 10) {
+        dy1 = -dy1;
+    }
+
+    
+    x1 += dx1;
+    y1 += dy1;
+}
+
+function clean_canvas(){
+    draw2();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    normal_ball.draw();
+    small_ball.draw();
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    //ctx.clearRect(10, 10, canvas.width, canvas.height);
+    //ctx.clearRect(x, y, 5, 5);
+    
+    //ctx.clearRect(x-5, y-5, 10, 10);
+    normal_ball.draw();
+
+    
     //drawBall_2();
     /*
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
@@ -163,4 +211,13 @@ function set_button_state(){
         button_state = 0;
     }
 }
-//setInterval(draw, 10);
+
+//使用addEventListener監聽器，監聽mousemove滑鼠移動，並觸發後面的function
+window.addEventListener('mousemove',(event) => {
+  /*在function內會傳入我們監聽的滑鼠物件，
+    我們可以從這個物件中取得我們要的資料：
+    x座標 event.pageX 及y座標 event.pageY
+    並把它印在console中*/
+  console.log(`${event.pageX},${event.pageY}`)
+})
+setInterval(clean_canvas, 10);
